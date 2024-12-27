a21587ac4c9b4842234c43124de33da2c3eac52e
Hiba Areej <syedahibaareej@gmail.com>
2024-12-27 10:34:34 +0500

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,28 +1,50 @@
 import React, { useState } from 'react';
 import swal from 'sweetalert';
 import styles from './Login.module.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Login = ({ switchToSignUp }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const handleLogin = (e) => {
     e.preventDefault();
-    swal.fire({
+    console.log('Email:', email);  // Check if the email state is being updated
+    console.log('Password:', password); // Check if the password state is being updated
+    swal({
       icon: 'success',
       title: 'Login Successful',
       text: 'Welcome back!'
     });
   };
+
   return (
     <div className={styles.form}>
       <h2>Log In</h2>
       <form onSubmit={handleLogin}>
         <label>Email:</label>
-        <input className={styles.inputField} placeholder='Enter your email' type="email" name="email" required />
+        <input
+          className={styles.inputField}
+          placeholder='Enter your email'
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
         <br /><br />
         <label>Password:</label>
-        <input className={styles.inputField} placeholder='Enter password' type="password" name="password" required/>
+        <input
+          className={styles.inputField}
+          placeholder='Enter password'
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
         <br /><br />
-        <button  className={styles.btn} type="submit">Log In</button>
+        <button className={styles.btn} type="submit">Log In</button>
       </form>
       <p>
         Don't have an account?{' '}
@@ -31,6 +53,5 @@ const Login = ({ switchToSignUp }) => {
     </div>
   );
 };
-
 
 export default Login;
