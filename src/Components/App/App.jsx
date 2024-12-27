9d77b72cd014284cdf611e3f1ca5d419b1efef14
Hiba Areej <syedahibaareej@gmail.com>
2024-12-27 10:09:58 +0500

    Update App.jsx

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -1,29 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Login from '../Login/Login';
 import Signup from '../Signup/Signup';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import image from '../../assets/user.png'
+import images from '../../assets/user.png'
 import styles from './App.module.css'
+
+
+// Main App Component
 const App = () => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const switchToSignUp = () => setIsLogin(false);
+  const switchToLogin = () => setIsLogin(true);
+
   return (
-    <Router>
-      <div className="container mt-5">
+    <div className="container mt-5 p-3">
       <div className={styles.container}>
-        <div className="row">
-          <div className="col-lg-6 col-md-6 col-sm-12 p-5">
-          <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-          </div>
-          <div className="col-lg-6 col-md-6 col-sm-12">
-            <img src={image} alt="" className={styles.image} />
-          </div>
+      <div className="row">
+        <div className="col-lg-6 col-sm-12 p-4">
+        {isLogin ? (
+        <Login switchToSignUp={switchToSignUp} />
+      ) : (
+        <Signup switchToLogin={switchToLogin} />
+      )}
+        </div>
+        <div className="col-lg-6 col-sm-12">
+          <img src={images} alt="" className={styles.image} />
         </div>
       </div>
     </div>
-    </Router>
+    </div>
   );
 };
 
